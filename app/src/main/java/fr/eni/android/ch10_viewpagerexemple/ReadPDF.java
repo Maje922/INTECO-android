dac293086de20fe26966dd425d1ebdb2a8c575ed
Maje <mjnct@hotmail.com>
2018-05-09 12:19:15 +0200

    arreglado xml

--- app/src/main/java/fr/eni/android/ch10_viewpagerexemple/ReadPDF.java
+++ app/src/main/java/fr/eni/android/ch10_viewpagerexemple/ReadPDF.java
@@ -1,7 +1,7 @@
 package fr.eni.android.ch10_viewpagerexemple;
 
 import android.app.Activity;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.net.Uri;
