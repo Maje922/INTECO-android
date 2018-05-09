6a641232c77bc37440cbb817d6bf474b8d8e4f8a
Maje <mjnct@hotmail.com>
2018-05-09 12:00:31 +0200

    arreglando error

--- app/src/main/java/fr/eni/android/ch10_viewpagerexemple/ReadPDF.java
+++ app/src/main/java/fr/eni/android/ch10_viewpagerexemple/ReadPDF.java
@@ -1,12 +1,12 @@
 package fr.eni.android.ch10_viewpagerexemple;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
@@ -84,7 +84,7 @@ public class ReadPDF extends Fragment implements View.OnClickListener {
                 cargarPDF();
                 break;
             case R.id.button6:
-                nombreFile = "DhsTarifas.pdf";
+                nombreFile = "DHsTarifas.pdf";
                 cargarPDF();
                 break;
             case R.id.floatingActionButton:
