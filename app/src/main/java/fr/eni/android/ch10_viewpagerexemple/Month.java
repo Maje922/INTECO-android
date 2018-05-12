83c755c3f14e7deb276d82af7382e9ad1a43acf8
Maje <mjnct@hotmail.com>
2018-05-12 22:21:11 +0200

    funcionan correctamente los spinners y calendario conjuntos en la busqueda de dia

--- app/src/main/java/fr/eni/android/ch10_viewpagerexemple/Month.java
+++ app/src/main/java/fr/eni/android/ch10_viewpagerexemple/Month.java
@@ -1,4 +1,20 @@
 package fr.eni.android.ch10_viewpagerexemple;
 
 public enum Month {
+    Enero, Febrero, Marzo, Abril, Mayo, Junio, Julio, Agosto, Septiembre, Octubre, Noviembre, diciembre;
+
+    public int maximoDias() {
+        switch (this) {
+            case Febrero:
+                return 29;
+            case Abril:
+            case Junio:
+            case Septiembre:
+            case Noviembre:
+                return 30;
+            default:
+                return 31;
+        }
+    }
+
 }
--- app/src/main/java/fr/eni/android/ch10_viewpagerexemple/dia.java
+++ app/src/main/java/fr/eni/android/ch10_viewpagerexemple/dia.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
@@ -20,7 +21,7 @@ import java.util.GregorianCalendar;
 /**
  * en este fragment se selecciona un dia para que muestre la grafica de ese día
  */
-public class dia extends Transiccion implements View.OnClickListener{
+public class dia extends Transiccion implements View.OnClickListener, AdapterView.OnItemSelectedListener{
 
     private CalendarView fecha;
     private Spinner dia;
@@ -28,6 +29,9 @@ public class dia extends Transiccion implements View.OnClickListener{
     private Spinner ano;
     private Button enviar;
     private Date diaSeleccion;
+    private Calendar c;
+    Month[] meses;
+
     public dia() {
         // Required empty public constructor
     }
@@ -47,33 +51,47 @@ public class dia extends Transiccion implements View.OnClickListener{
         mes = view.findViewById(R.id.mes);
         ano = view.findViewById(R.id.año);
 
+        //asignacion de eventos
+        dia.setOnItemSelectedListener(this);
+        mes.setOnItemSelectedListener(this);
+        ano.setOnItemSelectedListener(this);
 
         enviar.setOnClickListener(this);
 
+        //seleccion de fecha actual
+        c = Calendar.getInstance();
+        diaSeleccion = c.getTime();
+
+        meses = Month.values();
 
 
-        //valores de spinners
-        //Dias
 
-        //Long fechaL = fecha.getDate();
+        /*valores de spinners
 
+        A cada spiner se le asigna el valor correspondiente a la fecha actual al crear el fragment
+        */
 
-
-        //meses
-        adapter = ArrayAdapter.createFromResource(getActivity(), R.array.meses, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mes.setAdapter(adapter);
-
-        //años
+//años
 
         String[] anos = aniosSpinner();
         ano.setAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, anos));
 
 
+        //meses
+       spinnerMeses();
+
+        //Dias
+        spinnerDias(Calendar.getInstance().get(Calendar.MONTH));
+
+
         //detectamos un cambio en el calendario
         fecha.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-
+                //actualizamos los spinners
+                ano.setSelection(Calendar.getInstance().get(Calendar.YEAR)-year);
+                mes.setSelection(month);
+                dia.setAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, diasSpinner(meses[month].maximoDias())));
+                dia.setSelection(dayOfMonth-1);
                 diaSeleccion = new GregorianCalendar(year,month,dayOfMonth).getTime();
 
             }
@@ -100,5 +118,43 @@ public class dia extends Transiccion implements View.OnClickListener{
             anos[i] = String.valueOf(anoActual-i);
         }
         return anos;
+    }
+    //array para rellenar la cantidad de dias que mostrará el spinner
+    private String[] diasSpinner(int cantidadDias){
+        String[] dias = new String[cantidadDias];
+        for (int i = 0; i < dias.length; i ++){
+            dias[i] = String.valueOf(i+1);
+        }
+      return dias;
+    }
+
+
+    //al detectar un cambio en cualquier spinner se actualizan el dia seleccionado y el calendario
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        int a  = Integer.parseInt(ano.getSelectedItem().toString());
+        int m  = mes.getSelectedItemPosition();
+        int d = Integer.parseInt(dia.getSelectedItem().toString());
+
+         diaSeleccion = new GregorianCalendar(a,m,d).getTime();
+       fecha.setDate(diaSeleccion.getTime());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void spinnerMeses(){
+         int mesActual = Calendar.getInstance().get(Calendar.MONTH);
+        mes.setAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, meses));
+        mes.setSelection(mesActual);
+
+    }
+    public void spinnerDias(int mesActual){
+        int diasDelMesActual= meses[mesActual].maximoDias();
+        String[] valorSpinnerDias = diasSpinner(diasDelMesActual);
+        dia.setAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, valorSpinnerDias));
+        dia.setSelection(Calendar.getInstance().get(Calendar.DAY_OF_MONTH)-1);
     }
 }
